47893706e251682bc467c6a12e1da7edc9908c53
Aman <amanpushp11@gmail.com>
2026-04-20 21:58:54 +0530

    v

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.post('/api/login', async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
